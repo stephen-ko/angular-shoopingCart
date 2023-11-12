d5f746fbf70e8f9a1d25bf79e28783aed43ee9cf
stephen-ko <stephen.cj.ko@gmail.com>
2023-11-12 17:56:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-shoopingCart
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/stephen-ko/angular-shoopingCart)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { productsT } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  productsComp = [...productsT];
+
+  share() {
+    window.alert('button clicked, The product has been shared!');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
